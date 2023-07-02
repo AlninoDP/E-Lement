4bc888534c358b1daa1eb14115e8ec8e5cf23a13
Fumaru02 <120907247+Fumaru02@users.noreply.github.com>
2023-07-02 22:41:05 +0700

    menambah warna warna pelangi di color.dart

--- lib/models/colors.dart
+++ lib/models/colors.dart
@@ -16,6 +16,40 @@ class ChemistryColorApp {
   static const Color containerMenu9 = Color(0xffE8E7E4);
   static const Color containerMenu10 = Color(0xffFAE7D4);
   static const Color containerMenuOthers = Color(0xffE8E7E4);
+
+  // warna logam alkaline
   static const Color logamAlkaliContainer = Color(0xffCBE7FE);
   static const Color logamAlkaliText = Color(0xff0377FF);
+
+  // warna logam alkaline metals
+  static const Color logamAlkaliRedContainer = Color(0xffF4B7CE);
+  static const Color logamAlkaliRedContainerText = Color(0xffFF0060);
+
+  // warna logam pasca metals
+  static const Color pascaMetalsGreenContainer = Color(0xffD5FAE7);
+  static const Color pascaMetalsGreenText = Color(0xFF4B7158);
+
+  // warna logam Transition Metal
+  static const Color transitionMetalContainer = Color(0xffDFDFFF);
+  static const Color transitionMetalText = Color(0xff9376E0);
+
+  //warna NobleGases
+  static const Color nobleGasesContainer = Color(0xffFFE4EA);
+  static const Color nobleGasesText = Color(0xFFFF0060);
+
+  //warna metalloids
+  static const Color metalLoidsContainer = Color(0xffFDF6DE);
+  static const Color metalLoidsText = Color(0xffD1B065);
+
+  //warna re-active non metals
+  static const Color reActiveNonMetalsContainer = Color(0xffE4EDFC);
+  static const Color reActiveNonMetalsText = Color(0xff426ECE);
+
+  //warna Actinides
+  static const Color actinidesContainer = Color(0xffFCE7D4);
+  static const Color actinidesText = Color(0xffA84727);
+
+  //warna lanthanides
+  static const Color lanthanidesContainer = Color(0xffDFF3FD);
+  static const Color lanthanidesText = Color(0xff4B7158);
 }
