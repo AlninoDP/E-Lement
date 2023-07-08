c3a9bd9bcc0b8c92f946fef465b15d55040078a1
Fumaru02 <120907247+Fumaru02@users.noreply.github.com>
2023-07-08 22:57:11 +0700

    menambah dummy data pada unsur

---
+++ lib/models/data/alkali_data/alkali_data.dart
@@ -0,0 +1,15 @@
+class AlkaliData {
+  final String id;
+  final String title;
+  final String ingredient;
+  final String description;
+  final String category;
+
+  const AlkaliData({
+    required this.category,
+    required this.id,
+    required this.title,
+    required this.ingredient,
+    required this.description,
+  });
+}
---
+++ lib/models/data/category_item_grid.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoryItemGrid {
+  final String id;
+  final String title;
+  final Color containerColor;
+  final Color textColor;
+  final String symbol;
+
+  const CategoryItemGrid({
+    required this.id,
+    required this.title,
+    required this.containerColor,
+    required this.textColor,
+    required this.symbol,
+  });
+}
