b079545c7bbfe56b82608dcc3f215d5202477c57
Laptop Alnino <alnino007@gmail.com>
2024-01-09 11:38:23 +0700

    using getBuilder for auto dispose the controller

--- lib/view/main_menu/main_menu_screen.dart
+++ lib/view/main_menu/main_menu_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:kimiaapp/controller/search_bar_controller.dart';
-import 'package:kimiaapp/controller/main_controller.dart';
 import 'package:kimiaapp/components/background.dart';
 import 'package:kimiaapp/components/bubble_box.dart';
 import 'package:kimiaapp/view/main_menu/widgets/chemistry_fact.dart';
@@ -17,53 +16,53 @@ class MainMenuScreen extends StatelessWidget {
   final String guestName;
   MainMenuScreen({super.key, required this.guestName});
 
-  final MainController mainController = Get.put(MainController());
-
   final SearchBarController searchBarController =
       Get.put(SearchBarController());
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        top: true,
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: Background(
-              child: Scrollbar(
+      top: true,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Background(
+          child: Scrollbar(
             thickness: 8.r,
             radius: const Radius.circular(30).w,
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  BubbleBox(text: 'Hai,$guestName Nice to see you again...'),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Center(
-                    child: SearchBarElemen(
-                      controller: searchBarController.textSearchController,
-                      onChanged: searchBarController.searchElements,
-                    ),
-                  ),
-                  SearchResult(
-                      controller: searchBarController), //* SearchResult
-                  const GridMenu(),
-                  const Header3Menu(),
-                  const Center(
-                    child: ChemistryFact(),
-                  ),
-                  const Header4Menu(),
-                  const Center(
-                    child: ScientistQuote(),
-                  ),
-                ],
+              child: GetBuilder<SearchBarController>(
+                builder: (searchBarController) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 20.h),
+                      BubbleBox(
+                          text: 'Hai, $guestName. Nice to see you again...'),
+                      SizedBox(height: 20.h),
+                      Center(
+                        child: SearchBarElemen(
+                          controller: searchBarController.textSearchController,
+                          onChanged: searchBarController.searchElements,
+                        ),
+                      ),
+                      SearchResult(controller: searchBarController),
+                      const GridMenu(),
+                      const Header3Menu(),
+                      const Center(
+                        child: ChemistryFact(),
+                      ),
+                      const Header4Menu(),
+                      const Center(
+                        child: ScientistQuote(),
+                      ),
+                    ],
+                  );
+                },
               ),
             ),
-          )),
-        ));
+          ),
+        ),
+      ),
+    );
   }
 }
