5b5afe3067cc57a2d45f873e140416fecccf2c52
Laptop Alnino <alnino007@gmail.com>
2024-01-10 14:39:40 +0700

    factoring widget for more readibility code

--- lib/view/search_result/search_result_screen.dart
+++ lib/view/search_result/search_result_screen.dart
@@ -18,87 +18,81 @@ class SearchResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final MainController controller = Get.find();
     return SafeArea(
-        top: true,
-        child: Scaffold(
-          body: Background(
-            child: Scrollbar(
-              radius: const Radius.circular(30).w,
-              thickness: 10.w,
-              child: Center(
-                child: Column(
-                  children: [
-                    Expanded(
-                        child: ListView.builder(
-                            itemCount: listElemenData.length,
-                            itemBuilder: (ctx, index) {
-                              return Padding(
-                                padding: const EdgeInsets.all(10.0).w,
-                                child: Column(
-                                  children: [
-                                    SearchContentTitle(
-                                      containerColor:
-                                          ChemistryColorApp.containerTextColor,
-                                      title: listElemenData[index].title,
-                                      textColor: Colors.black,
-                                    ),
-                                    SizedBox(
-                                      height: 10.h,
-                                    ),
-                                    Container(
-                                      width: controller.screenWidth / 3,
-                                      height: controller.screenHeight / 6,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                          const Radius.circular(10).w,
-                                        ),
-                                        gradient: const LinearGradient(
-                                            colors: [
-                                              ChemistryColorApp
-                                                  .containerTextColor,
-                                              ChemistryColorApp.containerMenu2,
-                                            ],
-                                            begin: Alignment.topLeft,
-                                            end: Alignment.bottomRight),
-                                      ),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            listElemenData[index].title,
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.black),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 10.h,
-                                    ),
-                                    SearchContent1(
-                                        text: listElemenData[index]
-                                            .information[index]),
-                                    SizedBox(
-                                      height: 20.h,
-                                    ),
-                                    const SearchContentHeader(
-                                        text: "Description"),
-                                    SizedBox(
-                                      height: 8.h,
-                                    ),
-                                    SearchContent2(
-                                        text:
-                                            listElemenData[index].description),
-                                  ],
-                                ),
-                              );
-                            }))
-                  ],
-                ),
-              ),
+      top: true,
+      child: Scaffold(
+        body: Background(
+          child: Scrollbar(
+            radius: const Radius.circular(30).w,
+            thickness: 10.w,
+            child: ListView.separated(
+              itemCount: listElemenData.length,
+              separatorBuilder: (context, index) => SizedBox(height: 20.h),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.all(10.w),
+                  child: buildElementCard(controller, listElemenData[index]),
+                );
+              },
             ),
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  Widget buildElementCard(
+      MainController controller, ElementInformation element) {
+    return Column(
+      children: [
+        buildElementTitle(element),
+        SizedBox(height: 10.h),
+        buildElementContainer(controller, element),
+        SizedBox(height: 10.h),
+        SearchContent1(text: element.information.first),
+        SizedBox(height: 20.h),
+        const SearchContentHeader(text: "Description"),
+        SizedBox(height: 8.h),
+        SearchContent2(text: element.description),
+      ],
+    );
+  }
+
+  Widget buildElementTitle(ElementInformation element) {
+    return SearchContentTitle(
+      containerColor: ChemistryColorApp.containerTextColor,
+      title: element.title,
+      textColor: Colors.black,
+    );
+  }
+
+  Widget buildElementContainer(
+      MainController controller, ElementInformation element) {
+    return Container(
+      width: controller.screenWidth / 3,
+      height: controller.screenHeight / 6,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.w),
+        gradient: const LinearGradient(
+          colors: [
+            ChemistryColorApp.containerTextColor,
+            ChemistryColorApp.containerMenu2,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            element.title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
